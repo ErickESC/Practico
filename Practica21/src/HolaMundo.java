87e7f2b680e75ec292b094039c2e8bd075f0726f
eekos <eekos@LaMamalona>
2021-02-24 12:48:28 -0600

    First Commit

---
+++ Practica21/src/HolaMundo.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+
+/**
+ * @author eekos
+ *
+ */
+public class HolaMundo {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.print("Hola Mundo!");
+	}
+
+}
