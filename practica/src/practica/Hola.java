4a3a8010a06a0909f66e46c0bea036b8a84ddf7d
eekos <eekos@LaMamalona>
2021-02-23 22:35:23 -0600

    First Commit

---
+++ practica/src/practica/Hola.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package practica;
+
+/**
+ * @author eekos
+ *
+ */
+public class Hola {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.print("Hola Mundo!");
+	}
+
+}
